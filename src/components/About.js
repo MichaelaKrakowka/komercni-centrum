d6d89599c7bfafe3125c154d8a65fe1c36a00398
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-07-17 23:29:15 +0200

    working on background photo

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,13 @@ import "./About.css";
 
 export const About = () => {
   return (
-    <section>
-      <div className="main_headings">
-        <h1 className="first_heading">Podnikejte v centru dění </h1>
+    <section className="main_part">
+      <div className="main_part_blur">
+        {/* <div className="headings">
+          <h1 className="first_heading">Komerční centrum </h1>
+          <h2 className="first_heading">Nový Bohumín</h2>
+          <h3 className="second_heading">Podnikejte v centru dění</h3>
+        </div> */}
       </div>
     </section>
   );
--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,15 +1,30 @@
 import React from "react";
 import "./CompanyCard.css";
 
-export const CompanyCard = ({ name, phone, email, website }) => {
+export const CompanyCard = ({ name, subname, phone, email, website }) => {
   return (
     <div className="company_card">
-      <h2 className="company_name">{name}</h2>
-      <p className="company_phone">{phone}</p>
-      <p className="company_email">{email}</p>
-      <a href={website} className="company_website">
-        {website}
-      </a>
+      <h3 className="company_name">{name}</h3>
+      <h4 className="company_name">{subname}</h4>
+
+      <p className="company_phone">
+        <span className="material-symbols-outlined">call</span>
+        {phone}
+      </p>
+      <p className="company_email">
+        <span className="material-symbols-outlined">alternate_email</span>
+        {email}
+      </p>
+      <div className="company_website">
+        <a
+          href={website.startsWith("http") ? website : `https://${website}`}
+          target="_blank"
+          rel="noopener noreferrer">
+          {" "}
+          <span className="material-symbols-outlined">home</span>
+          {website}
+        </a>
+      </div>
     </div>
   );
 };

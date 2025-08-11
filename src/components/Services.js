f3f551248c8aa1df8520a3b18bc3d898159603f8
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-08-11 19:45:56 +0200

    building filter

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,13 +5,28 @@ import { companies } from "../data/companies";
 import { RevealSection } from "./RevealSection";
 
 export const Services = () => {
+  const [building, setBuilding] = React.useState("Komenskeho");
+
+  const filteredCompanies = companies.filter(
+    (company) => company.id === building
+  );
+
   return (
     <section id="services">
       <RevealSection>
         <h2 className="secondary_heading fade_item delay_0">Služby</h2>
+        <div className="buildings_filter">
+          <select
+            className="building"
+            value={building}
+            onChange={(company) => setBuilding(company.target.value)}>
+            <option value="Komenskeho">Komenského ulice</option>
+            <option value="Kostelni">Kostelní ulice</option>
+          </select>
+        </div>
 
         <div className="company_cards ">
-          {companies.map((company, index) => (
+          {filteredCompanies.map((company, index) => (
             <CompanyCard key={index} {...company} />
           ))}
         </div>
---
+++ src/data/whyUs.js
@@ -0,0 +1,14 @@
+{
+  /* <button
+            type="button"
+            className="btn fade_item delay_1"
+            onClick={() => setBuilding("Komenskeho")}>
+            Komenského ulice
+          </button>
+          <button
+            type="button"
+            className="btn fade_item delay_2"
+            onClick={() => setBuilding("Kostelni")}>
+            Kostelní ulice
+          </button> */
+}

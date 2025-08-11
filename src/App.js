9b7b6a51f28922b0855119924d4ce9fae8e5cd3b
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-08-11 20:20:34 +0200

    working on select root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { MainSection } from "./components/MainSection";
-import { Footer } from "./components/Footer";
+// import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
--- src/components/Services.js
+++ src/components/Services.js
@@ -3,6 +3,7 @@ import "./Services.css";
 import { CompanyCard } from "./CompanyCard";
 import { companies } from "../data/companies";
 import { RevealSection } from "./RevealSection";
+import * as Select from "@radix-ui/react-select"; //filtrovani budov
 
 export const Services = () => {
   const [building, setBuilding] = React.useState("Komenskeho");
@@ -16,13 +17,26 @@ export const Services = () => {
       <RevealSection>
         <h2 className="secondary_heading fade_item delay_0">Služby</h2>
         <div className="buildings_filter">
-          <select
-            className="building"
-            value={building}
-            onChange={(company) => setBuilding(company.target.value)}>
-            <option value="Komenskeho">Komenského ulice</option>
-            <option value="Kostelni">Kostelní ulice</option>
-          </select>
+          <Select.Root value={building} onValueChange={setBuilding}>
+            <Select.Trigger className="rselect_trigger" aria-label="Budova">
+              <Select.Value />
+            </Select.Trigger>
+            <Select.Portal>
+              <Select.Content
+                className="rselect_content"
+                position="popper" //nastavi, aby se mi menu zobrazovalo bud nahore nebo dole, podle toho kde je misto
+                sideOffset={6}>
+                <Select.Viewport className="rselect_viewport">
+                  <Select.Item className="rselect_item" value="Komenskeho">
+                    <Select.ItemText>Komenského ulice</Select.ItemText>
+                  </Select.Item>
+                  <Select.Item className="rselect_item" value="Kostelni">
+                    <Select.ItemText>Kostelní ulice</Select.ItemText>
+                  </Select.Item>
+                </Select.Viewport>
+              </Select.Content>
+            </Select.Portal>
+          </Select.Root>
         </div>
 
         <div className="company_cards ">

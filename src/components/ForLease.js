0dd27a7ca0c325031b61a85dee6acb01b29b2962
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-08-15 00:36:06 +0200

    working on ForLease part

--- src/components/ForLease.js
+++ src/components/ForLease.js
@@ -1,11 +1,31 @@
 import "./ForLease.css";
 import { RevealSection } from "./RevealSection";
+import { advantage } from "../data/advantage";
 
 export const ForLease = () => {
   return (
     <section id="forLease">
       <RevealSection>
-        <h2 className="secondary_heading fade_item delay_0">Proč u nás</h2>
+        <h2 className="secondary_heading fade_item delay_0">
+          Volné prostory k podnikání
+        </h2>
+        <h3
+          className="fade_item delay_1"
+          style={{ textAlign: "center", fontSize: "24px" }}>
+          Proč podnikat u nás
+        </h3>
+
+        <div className="advantage_container ">
+          {advantage.map((item) => (
+            <div key={item.id} className="advantage_card fade_item delay_2">
+              <span className="material-symbols-outlined symbols_advantage">
+                {item.iconName}
+              </span>
+              <h3>{item.title}</h3>
+              <p>{item.paragraph}</p>
+            </div>
+          ))}
+        </div>
       </RevealSection>
     </section>
   );
---
+++ src/data/advantage.js
@@ -0,0 +1,34 @@
+export const advantage = [
+  {
+    id: 1,
+    iconName: "architecture",
+
+    title: "Historické budovy",
+    paragraph:
+      "Podnikejte v budovách s jedinečnou historií a charakterem, které dodají vašemu podnikání prestiž a styl.",
+  },
+
+  {
+    id: 2,
+    iconName: "location_on",
+    title: "Centrum města",
+    paragraph:
+      "V srdci Bohumína máte vše po ruce – úřady, obchody, restaurace a klidné náměstí pro odpočinek během dne.",
+  },
+
+  {
+    id: 3,
+    iconName: "commute",
+    title: "Skvělá dostupnost",
+    paragraph:
+      "Jen pár minut chůze od autobusové i vlakové stanice, takže vaši zákazníci a zaměstnanci vás snadno najdou.",
+  },
+
+  {
+    id: 4,
+    iconName: "parking_sign",
+    title: "Parkování",
+    paragraph:
+      "K dispozici je jedno parkovací místo na soukromém pozemku spadající k budouvě na Komenského ulici a další parkování je možné na veřejných parkovištích před i za oběma budovami.",
+  },
+];

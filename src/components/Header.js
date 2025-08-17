4164065eccfb70bfbc001f06acdfd71258ada7dd
Michaela Krakowka <michaela.krakowka@gmail.com>
2025-08-17 17:42:23 +0200

    header nav done for desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,37 @@ export const Header = () => {
         <a href="/">
           <img className="logo" src="/logoKC.png" alt="logo" />
         </a>
+
         <nav>
+          <ul className="nav_links">
+            <li className="nav_element">
+              <a href="#about" className="nav_link">
+                O nás
+              </a>
+            </li>
+            <li className="nav_element">
+              <a href="#services" className="nav_link">
+                Služby
+              </a>
+            </li>
+            <li className="nav_element">
+              <a href="#forLease" className="nav_link">
+                Proč u nás
+              </a>
+            </li>
+            <li className="nav_element">
+              <a href="#form" className="nav_link">
+                Kontakt
+              </a>
+            </li>
+            <li className="nav_element">
+              <a href="#map" className="nav_link">
+                Kde nás najdete
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="nav_hamburger">
           <HamburgerMenu />
         </nav>
       </div>
